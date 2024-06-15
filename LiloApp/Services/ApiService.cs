39cb18dab8075d65d973bc6aeaea837d5d8c0b57
carincon93 <carincon93@gmail.com>
2024-06-15 11:38:13 -0500

    ApiService added

---
+++ LiloApp/Services/ApiService.cs
@@ -0,0 +1,27 @@
+﻿using LiloApp.Models;
+using Newtonsoft.Json;
+
+namespace LiloApp.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Dream>> GetDreamsAsync()
+        {
+            var response = await _httpClient.GetAsync("https://liloapp-backend.onrender.com/dreams");
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<List<Dream>>(json);
+            }
+
+            return new List<Dream>();
+        }
+    }
+}
