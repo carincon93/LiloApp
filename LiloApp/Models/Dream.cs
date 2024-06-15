afacba72e629483412d435df20d96345178180c3
carincon93 <carincon93@gmail.com>
2024-06-15 11:37:43 -0500

    Dream Model & ViewModel added

---
+++ LiloApp/Models/Dream.cs
@@ -0,0 +1,9 @@
+﻿namespace LiloApp.Models
+{
+    public class Dream
+    {
+        public string Id { get; set; }
+        public string dream { get; set; }
+        public string userId { get; set; }
+    }
+}
---
+++ LiloApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using LiloApp.Models;
+using LiloApp.Services;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LiloApp.ViewModels
+{
+    public class MainViewModel
+    {
+        private readonly ApiService _apiService;
+        private List<Dream> _dreams;
+
+        public List<Dream> Dreams
+        {
+            get => _dreams;
+            set
+            {
+                _dreams = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel(ApiService apiService)
+        {
+            _apiService = apiService;
+            LoadDreamsAsync().ConfigureAwait(false);
+        }
+
+        public async Task LoadDreamsAsync()
+        {
+            Dreams = await _apiService.GetDreamsAsync();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
