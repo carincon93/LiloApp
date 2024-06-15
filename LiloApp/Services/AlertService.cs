d3c4212a3d5415a68917049fedfba776693d8b95
carincon93 <carincon93@gmail.com>
2024-06-15 11:38:01 -0500

    Refactor

--- LiloApp/Services/AlertService.cs
+++ LiloApp/Services/AlertService.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading.Tasks;
-using Microsoft.Maui.Controls;
-
-namespace LiloApp.Services
+﻿namespace LiloApp.Services
 {
     public interface IAlertService
     {
