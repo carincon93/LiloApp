4960512e66ea29abcf2dd800ccf10b31fa55b0a0
carincon93 <carincon93@gmail.com>
2024-06-16 16:31:55 -0500

    LocalDatabaseServices refactorized

---
+++ LiloApp/Services/LocalDatabaseService.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace LiloApp.Services
+{
+	public class LocalDatabaseService
+	{
+		public SQLiteAsyncConnection Database { get; }
+
+		public LocalDatabaseService(string dbPath)
+		{
+			Database = new SQLiteAsyncConnection(dbPath);
+		}
+	}
+}
