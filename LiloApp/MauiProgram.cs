528c59740a36d1987c932257a59dbd567f78791d
carincon93 <carincon93@gmail.com>
2024-06-16 16:30:43 -0500

    Sqli and Data services added

--- LiloApp/MauiProgram.cs
+++ LiloApp/MauiProgram.cs
@@ -2,6 +2,7 @@
 using LiloApp.Services;
 using Microsoft.Extensions.Logging;
 using LiloApp.ViewModels;
+using SQLite;
 
 namespace LiloApp
 {
@@ -27,9 +28,20 @@ namespace LiloApp
             builder.Services.AddBlazorWebViewDeveloperTools();
 
             builder.Services.AddSingleton<NavigatorService>();
-            builder.Services.AddSingleton<HttpClient>();
-            builder.Services.AddSingleton<ApiService>();
-            builder.Services.AddSingleton<MainViewModel>();
+
+			// Register SQLite connection
+			string dbPath = Path.Combine(FileSystem.AppDataDirectory, "LiloApp.db3");
+			builder.Services.AddSingleton<SQLiteAsyncConnection>(new SQLiteAsyncConnection(dbPath));
+
+			// Register services
+			builder.Services.AddSingleton<LocalDatabaseService>();
+			builder.Services.AddSingleton<DreamService>();
+			builder.Services.AddSingleton<DreamCalendarService>();
+			builder.Services.AddSingleton<GrupoMuscularService>();
+			builder.Services.AddSingleton<MainViewModel>();
+
+            //builder.Services.AddSingleton<HttpClient>();
+            //builder.Services.AddSingleton<ApiService>();
 
             return builder.Build();
         }
