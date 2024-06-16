081b8e78203d0bc579f31a26a0281b58694da37a
carincon93 <carincon93@gmail.com>
2024-06-16 16:31:37 -0500

    New services added

---
+++ LiloApp/Services/DreamCalendarService.cs
@@ -0,0 +1,38 @@
+﻿using LiloApp.Data;
+using SQLite;
+
+namespace LiloApp.Services
+{
+	public class DreamCalendarService
+	{
+		private readonly SQLiteAsyncConnection _database;
+
+		public DreamCalendarService(SQLiteAsyncConnection database)
+		{
+			_database = database;
+			_database.CreateTableAsync<DreamCalendarData>().Wait();
+		}
+
+		public Task<List<DreamCalendarData>> GetDreamCalendarAsync()
+		{
+			return _database.Table<DreamCalendarData>().ToListAsync();
+		}
+
+		public Task<int> SaveDreamCalendarAsync(DreamCalendarData dreamCalendar)
+		{
+			if (dreamCalendar.Id != 0)
+			{
+				return _database.UpdateAsync(dreamCalendar);
+			}
+			else
+			{
+				return _database.InsertAsync(dreamCalendar);
+			}
+		}
+
+		public Task<int> DeleteDreamCalendarAsync(DreamCalendarData dreamCalendar)
+		{
+			return _database.DeleteAsync(dreamCalendar);
+		}
+	}
+}
---
+++ LiloApp/Services/DreamService.cs
@@ -0,0 +1,38 @@
+﻿using LiloApp.Data;
+using SQLite;
+
+namespace LiloApp.Services
+{
+	public class DreamService
+	{
+		private readonly SQLiteAsyncConnection _database;
+
+		public DreamService(SQLiteAsyncConnection database)
+		{
+			_database = database;
+			_database.CreateTableAsync<DreamData>().Wait();
+		}
+
+		public Task<List<DreamData>> GetDreamsAsync()
+		{
+			return _database.Table<DreamData>().ToListAsync();
+		}
+
+		public Task<int> SaveDreamAsync(DreamData dream)
+		{
+			if (dream.Id != 0)
+			{
+				return _database.UpdateAsync(dream);
+			}
+			else
+			{
+				return _database.InsertAsync(dream);
+			}
+		}
+
+		public Task<int> DeleteDreamAsync(DreamData dream)
+		{
+			return _database.DeleteAsync(dream);
+		}
+	}
+}
