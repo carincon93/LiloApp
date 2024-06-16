8c97034954a67c6c1aaad1a8f7fae17fd3c4a8af
carincon93 <carincon93@gmail.com>
2024-06-16 16:30:24 -0500

    Remove models folder

--- LiloApp/Models/Dream.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LiloApp.Models
-{
-    public class Dream
-    {
-        public string Id { get; set; }
-        public string dream { get; set; }
-        public string userId { get; set; }
-    }
-}
