0923fdf00cd18638d2ec3e7c7ff26d2031262f44
carincon93 <carincon93@gmail.com>
2024-06-19 14:58:12 -0500

    change app colors

--- LiloApp/Platforms/Android/MainActivity.cs
+++ LiloApp/Platforms/Android/MainActivity.cs
@@ -1,11 +1,26 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 
 namespace LiloApp
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                Window.SetStatusBarColor(Android.Graphics.Color.White);
+
+                if (Build.VERSION.SdkInt >= BuildVersionCodes.M)
+                {
+                    Window.DecorView.SystemUiVisibility = (StatusBarVisibility)SystemUiFlags.LightStatusBar;
+                }
+            }
+        }
     }
 }
