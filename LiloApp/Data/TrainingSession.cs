f5023b807a049ef0c9656bf9bace01d090752bdc
carincon93 <carincon93@gmail.com>
2024-06-19 12:10:06 -0500

    Date added

--- LiloApp/Data/TrainingSession.cs
+++ LiloApp/Data/TrainingSession.cs
@@ -11,18 +11,24 @@ namespace LiloApp.Data
 		public int Id { get; set; }
 
 		[Column("set")]
-		public int Set { get; set; }
+		public int? Set { get; set; }
 
 		[Column("reps")]
-		public int Reps { get; set; }
+		public int? Reps { get; set; }
 
 		[Column("weight")]
-		public int Weight { get; set; }
+		public int? Weight { get; set; }
 
 		[Column("exerciseId")]
 		public int ExerciseId { get; set; }
 
-		[Column("date")]
-		public DateTime Date { get; set; }
+		[Column("dayNumber")]
+		public int DayNumber { get; set; }
+
+		[Column("monthNumber")]
+		public int MonthNumber { get; set; }
+
+		[Column("year")]
+		public int Year { get; set; }
 	}
 }
