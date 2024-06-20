580e112543636e50fa36367c351fba44da7f8276
carincon93 <carincon93@gmail.com>
2024-06-20 00:13:40 -0500

    PetLife added

---
+++ LiloApp/Data/PetLifeData.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+
+namespace LiloApp.Data
+{
+    [Table("PetLifeData")]
+    public class PetLifeData
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("dayNumber")]
+        public int DayNumber { get; set; }
+
+        [Column("diff")]
+        public int Diff { get; set; }
+    }
+}
---
+++ LiloApp/Services/PetLifeService.cs
@@ -0,0 +1,38 @@
+﻿using LiloApp.Data;
+using SQLite;
+
+namespace LiloApp.Services
+{
+    public class PetLifeService
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public PetLifeService(SQLiteAsyncConnection database)
+        {
+            _database = database;
+            _database.CreateTableAsync<PetLifeData>().Wait();
+        }
+
+        public Task<List<PetLifeData>> GetPetLifeAsync()
+        {
+            return _database.Table<PetLifeData>().ToListAsync();
+        }
+
+        public Task<int> SavePetLifeAsync(PetLifeData petLife)
+        {
+            if (petLife.Id != 0)
+            {
+                return _database.UpdateAsync(petLife);
+            }
+            else
+            {
+                return _database.InsertAsync(petLife);
+            }
+        }
+
+        public Task<int> DeletePetLifeAsync(PetLifeData petLife)
+        {
+            return _database.DeleteAsync(petLife);
+        }
+    }
+}
