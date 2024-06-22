9901397c75f86231d0e37e71f0570612bfbaefa8
carincon93 <carincon93@gmail.com>
2024-06-22 12:27:41 -0500

    Refactor code

--- LiloApp/wwwroot/js/site.js
+++ LiloApp/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-window.addBodyNoScroll = () => {
+window.addBodyBlurPopup = () => {
     document.body.classList.add('blur-popup');
 
     // Find all elements with class 'slide-left' and add class 'slide-left-x' and 'relative'
@@ -8,10 +8,26 @@ window.addBodyNoScroll = () => {
         element.classList.add('slide-left-x');
         element.classList.add('relative');
     });
+
+    // Find all elements with class 'slide-right' and add class 'slide-right-x' and 'relative'
+    const slideRightElements = document.querySelectorAll('.slide-right');
+    slideRightElements.forEach(element => {
+        element.classList.remove('slide-right');
+        element.classList.add('slide-right-x');
+        element.classList.add('relative');
+    });
 };
 
-window.removeBodyNoScroll = () => {
+window.removeBodyBlurPopup = () => {
     document.body.classList.remove('blur-popup');
+
+    // Find all elements with class 'slide-right-x' and revert to 'slide-right'
+    const slideRightXElements = document.querySelectorAll('.slide-right-x');
+    slideRightXElements.forEach(element => {
+        element.classList.remove('slide-right-x');
+        element.classList.add('slide-right');
+        element.classList.remove('relative');
+    });
 
     // Find all elements with class 'slide-left-x' and revert to 'slide-left'
     const slideLeftXElements = document.querySelectorAll('.slide-left-x');
